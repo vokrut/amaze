3ee6c89c086040a9df4bb42f2e77735b1f42be40
Dmytro Turkov <dmytrot@tabtale.com>
2019-05-16 17:23:49 +0300

    process levels

---
+++ src/com/kengoweb/amaze/Level.java
@@ -0,0 +1,4 @@
+package com.kengoweb.amaze;
+
+public class Level {
+}
--- src/com/kengoweb/amaze/Main.java
+++ src/com/kengoweb/amaze/Main.java
@@ -1,9 +1,43 @@
 package com.kengoweb.amaze;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+        File amazeDir = new File("src/amaze_levels");
+        if (!amazeDir.exists()) {
+            System.out.println("Folder amaze_levels not found");
+        } else {
+            processLevelFiles(amazeDir.listFiles());
+        }
+    }
+
+    private static void processLevelFiles(File[] levelFiles) {
+        System.out.println("processLevelFiles");
+        if (!checkLevelsFolder(levelFiles)) {
+            return;
+        }
+        Arrays.sort(levelFiles);
+        for (File fileLevel : levelFiles) {
+            if (fileLevel.isDirectory()) {
+                continue;
+            }
+            System.out.println(fileLevel.getName());
+        }
+    }
+
+    private static boolean checkLevelsFolder(File[] levelFiles) {
+        if (levelFiles == null) {
+            System.out.println("Can't get levels from folder");
+            return false;
+        } else if (levelFiles.length == 0) {
+            System.out.println("Level folders is empty");
+            return false;
+        }
+        return true;
     }
 }
